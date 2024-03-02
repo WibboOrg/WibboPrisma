184a795f5fc4653fc12642aff2396651b00a0714
Kodamas <superjason95@gmail.com>
2024-03-02 20:48:03 +0100

    fix: remove export prisma default

--- dist/index.js
+++ dist/index.js
@@ -14,5 +14,4 @@ var __exportStar = (this && this.__exportStar) || function(m, exports) {
     for (var p in m) if (p !== "default" && !Object.prototype.hasOwnProperty.call(exports, p)) __createBinding(exports, m, p);
 };
 Object.defineProperty(exports, "__esModule", { value: true });
-exports.prisma = void 0;
 __exportStar(require("./client/index"), exports);
